b6d29404599212d0d0eb0410a5d502905ac75853
vale <valefeliz1307@gmail.com>
2018-02-19 13:49:28 -0600

    primer commit
    soy vale y este es mi primer commit

---
+++ PopoGo/PopoGo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PopoGo
+//
+//  Created by Vale Machado on 19/02/18.
+//  Copyright © 2018 Vale.Machado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
