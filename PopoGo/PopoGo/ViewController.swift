ea0696b154782309531be0a9b5b1e787698f7771
vale <valefeliz1307@gmail.com>
2018-02-21 11:30:15 -0600

    mapa locochon sabe donde hay popo
    fin

--- PopoGo/PopoGo/ViewController.swift
+++ PopoGo/PopoGo/ViewController.swift
@@ -9,22 +9,33 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
     var manager = CLLocationManager()
     
     @IBOutlet weak var mapaCool: MKMapView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       
+        self.manager.delegate = self
+        
+        if CLLocationManagerDelegate.auto == .authorizedWhenInUse {
+            print("Estamos listos para salir a casar popos")
+        }
+        }
+        
+        self.manager.requestWhenInUseAuthorization()
+        
             self.mapaCool.showsUserLocation = true
+       
         // Do  any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
+func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
 
-}
+
 
